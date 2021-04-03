a61661bcbc32bf9960a8887c7c9dc81fd2ed7d87
Alberto <agonzalez@tribalyte.com>
2021-04-03 23:14:33 +0200

    Add and configure sass
    - Configure sass in the project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import './Home.scss';
 
 const Home: React.FC = () => {
   return (
