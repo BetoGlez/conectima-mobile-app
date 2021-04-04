382c865111efb3c9e3160228c17a747d88c41728
Alberto <agonzalez@tribalyte.com>
2021-04-05 00:04:56 +0200

    Add logo to the login page
    - Include app name and logo to the login page

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import "./LoginPage.scss"
 import AppConfig from "../../app-constants";
 import AuthBoxComponent from "./AuthBoxComponent/AuthBoxComponent";
 
 const LoginPage: React.FC = () => {
+
+    const { t } = useTranslation();
+
     return (
         <div className="login-page" style={{ backgroundImage: `url(${AppConfig.LOGIN_BG_IMAGE_URL})` }}>
+            <img className="conectima-logo" src="/assets/images/conectima-logo.png" alt="Conectima logo" />
+            <h1 className="app-name">{t("general.appName")}</h1>
             <AuthBoxComponent />
         </div>
     );
