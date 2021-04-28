74e6f231f72fcb4d51daac1b7681b90096bf9def
Alberto <agonzalez@tribalyte.com>
2021-04-29 01:04:23 +0200

    Minor translations file refactor
    - Remove quotes from the translations structure

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -1,59 +1,59 @@
 export const en = {
     translation: {
-        "general": {
-            "appName": "Conectima",
-            "start": "Start",
-            "on": "On",
-            "off": "Off",
-            "noData": "No data available",
-            "select": "Select",
-            "cancel": "Cancel"
+        general: {
+            appName: "Conectima",
+            start: "Start",
+            on: "On",
+            off: "Off",
+            noData: "No data available",
+            select: "Select",
+            cancel: "Cancel"
         },
-        "errors": {
-            "userNotFound": "There is no user registered with this credentials",
-            "invalidCredentials": "The credentials you are using are incorrect"
+        errors: {
+            userNotFound: "There is no user registered with this credentials",
+            invalidCredentials: "The credentials you are using are incorrect"
         },
-        "pages": {
-            "projects": "Projects",
-            "compare": "Compare",
-            "analytics": "Analytics",
-            "configure": "Configure"
+        pages: {
+            projects: "Projects",
+            compare: "Compare",
+            analytics: "Analytics",
+            configure: "Configure"
         },
-        "auth": {
-            "introduceCredentials": "Introduce your credentials",
-            "email": "Email",
-            "password": "Password",
-            "login": "Login",
-            "logout": "Logout"
+        auth: {
+            introduceCredentials: "Introduce your credentials",
+            email: "Email",
+            password: "Password",
+            login: "Login",
+            logout: "Logout"
         },
-        "roles": {
-            "iAm": "I am a",
-            "manager": "Project manager",
-            "developer": "Software developer"
+        roles: {
+            iAm: "I am a",
+            manager: "Project manager",
+            developer: "Software developer"
         },
-        "projects": {
-            "projects": "Projects",
-            "dedication": "Dedication",
-            "startedOn": "Started on {{startDate}}",
-            "ongoingFor": "Ongoing for {{dayNumber}} days",
-            "editProject": "Edit Project"
+        projects: {
+            projects: "Projects",
+            dedication: "Dedication",
+            startedOn: "Started on {{startDate}}",
+            ongoingFor: "Ongoing for {{dayNumber}} days",
+            editProject: "Edit Project"
         },
-        "sprints": {
-            "selectSprint": "Select a sprint",
-            "changeSprint": "Change sprint",
-            "sprintVersion": "Sprint {{sprintVersion}}",
-            "issues": "{{issueNumber}} issues",
-            "sp": "{{sp}} sp",
-            "duration": "{{days}} days - {{hoursPerDay}}h / day"
+        sprints: {
+            selectSprint: "Select a sprint",
+            changeSprint: "Change sprint",
+            sprintVersion: "Sprint {{sprintVersion}}",
+            issues: "{{issueNumber}} issues",
+            sp: "{{sp}} sp",
+            duration: "{{days}} days - {{hoursPerDay}}h / day"
         },
-        "dedication": {
-            "percentage": "{{percentage}}%",
-            "hoursPerDay": "{{hours}}h / day",
-            "noDedications": "No user dedication data"
+        dedication: {
+            percentage: "{{percentage}}%",
+            hoursPerDay: "{{hours}}h / day",
+            noDedications: "No user dedication data"
         },
-        "analytics": {
-            "projectAnalytics": "Project analytics",
-            "seeAnalytics": "See analytics"
+        analytics: {
+            projectAnalytics: "Project analytics",
+            seeAnalytics: "See analytics"
         },
         charts: {
             burndown: {
@@ -65,14 +65,14 @@ export const en = {
                 cardDescription: "A graphical representation of the work left to do versus time"
             }
         },
-        "configure": {
-            "settings": "Settings",
-            "general": "General",
-            "devCostRate": "Developer cost rate",
-            "emailNotifications": "Email notifications",
-            "appInfo": "App information",
-            "version": "Version",
-            "more": "More"
+        configure: {
+            settings: "Settings",
+            general: "General",
+            devCostRate: "Developer cost rate",
+            emailNotifications: "Email notifications",
+            appInfo: "App information",
+            version: "Version",
+            more: "More"
         }
     }
 };
